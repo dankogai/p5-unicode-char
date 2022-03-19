445a125eb7c0ae735f6f55ae63623d1214c25cfa
Dan Kogai <dankogai+github@gmail.com>
2022-03-19 14:09:59 +0900

    init travis-ci

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+
+[![build status](https://app.travis-ci.com/dankogai/p5-unicode-char.svg)](https://app.travis-ci.com/github/dankogai/p5-p5-unicode-char)
+
 # p5-unicode-char
+
 Unicode::Char - OO interface to charnames and others
+
+See https://metacpan.org/pod/Unicode::Char for details.
--- lib/Unicode/Char.pm
+++ lib/Unicode/Char.pm
@@ -150,11 +150,11 @@ L<perlunicode>, L<perluniintro>, L<charnames>
 
 =head1 AUTHOR
 
-Dan Kogai, E<lt>dankogai@dan.co.jp<gt>
+Dan Kogai, E<lt>dankogai@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2006 by Dan Kogai
+Copyright (C) 2006-2022 by Dan Kogai
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.8 or,
